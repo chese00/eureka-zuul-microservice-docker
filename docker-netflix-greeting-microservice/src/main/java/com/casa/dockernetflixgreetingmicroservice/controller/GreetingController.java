ab7c110988fc56b37c0100a27a1edf0f563e749e
chese00 <chese00@gmail.com>
2022-05-08 00:39:01 -0500

    Fixing some issues, but still doesn't work totally.

--- docker-netflix-greeting-microservice/src/main/java/com/casa/dockernetflixgreetingmicroservice/controller/GreetingController.java
+++ docker-netflix-greeting-microservice/src/main/java/com/casa/dockernetflixgreetingmicroservice/controller/GreetingController.java
@@ -4,9 +4,12 @@ package com.casa.dockernetflixgreetingmicroservice.controller;
 import com.casa.dockernetflixgreetingmicroservice.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
+@RequestMapping("greeting-microservice")
 public class GreetingController {
 
     GreetingService greetingService;
